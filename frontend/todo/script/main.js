c06e950819f3123ceddd4c0f36d0555fca02e417
RohitSignIn <rohitsinghsignin@gmail.com>
2024-03-02 18:10:53 +0530

    Todo UI with some interaction

--- frontend/todo/script/main.js
+++ frontend/todo/script/main.js
@@ -1,69 +1,47 @@
-let data = {
-  username: "Rohit",
-};
+const todoInput = document.getElementById("todoInput");
+const addTodo = document.getElementById("addTodoBtn");
 
-$.ajax({
-  type: "get",
-  url: "http://localhost:9090/api/v1/todos",
-  success: function (response) {
-    // console.log(response);
-    for (let i = 0; i < response["res"].length; i++) {
-      paintUI(response["res"][i][1]);
-    }
-  },
+const todoCon = document.getElementById("todoCon");
+
+addTodo.addEventListener("click", function () {
+  const task = todoInput.value;
+
+  const para = document.createElement("p");
+  para.textContent = task;
+
+  const statusBtn = document.createElement("button");
+  statusBtn.textContent = "PENDING";
+  statusBtn.classList.add("btnPend");
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "DELETE";
+  statusBtn.classList.add("btnDel");
+
+  paraCon = document.createElement("div");
+  statusCon = document.createElement("div");
+  deleteCon = document.createElement("div");
+
+  paraCon.append(para);
+  statusCon.append(statusBtn);
+  deleteCon.append(deleteBtn);
+
+  taskCon = document.createElement("div");
+  taskCon.append(paraCon);
+  taskCon.append(statusCon);
+  taskCon.append(deleteCon);
+  taskCon.classList.add("todo");
+
+  todoCon.append(taskCon);
+
+  // <div class='todo'>
+  //   <div>
+  //     <p>Task ...</p>
+  //   </div>
+  //   <div>
+  //     <button class='btnPend'>PENDING</button>
+  //   </div>
+  //   <div>
+  //     <button class='btnDel'>DELETE</button>
+  //   </div>
+  // </div>;
 });
-
-let status = document.getElementById("persona");
-
-let addTodo = document.getElementById("addTodo");
-let todoInput = document.getElementById("todoInput");
-let todoCon = document.getElementById("todoCon");
-
-addTodo.addEventListener("click", () => {
-  // todoInput.value
-  // Check how to convert in FormData
-
-  //   $.ajax({
-  //     type: "POST",
-  //     url: "http://localhost:9090/api/v1/todos",
-  //     data: ,
-  //     success: function (response) {
-  //       paintUI(todoInput.value);
-  //     },
-  //   });
-
-  paintUI(todoInput.value);
-});
-
-function paintUI(task) {
-  todo = document.createElement("div");
-  todoPara = document.createElement("p");
-  btnCon = document.createElement("div");
-  statusBtn = document.createElement("button");
-  deleteBtn = document.createElement("button");
-
-  todo.classList.add("d-flex");
-  todo.classList.add("gap-2");
-  todoPara.classList.add("flex-grow-1");
-  todoPara.classList.add("border");
-  todoPara.classList.add("p-2");
-
-  todoPara.textContent = task;
-
-  statusBtn.classList.add("btn");
-  statusBtn.classList.add("btn-warning");
-  statusBtn.textContent = "Completed";
-
-  deleteBtn.classList.add("btn");
-  deleteBtn.classList.add("ms-2");
-  deleteBtn.classList.add("btn-danger");
-  deleteBtn.textContent = "Delete";
-
-  btnCon.appendChild(statusBtn);
-  btnCon.appendChild(deleteBtn);
-
-  todo.appendChild(todoPara);
-  todo.appendChild(btnCon);
-
-  todoCon.appendChild(todo);
-}
