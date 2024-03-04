b502240724d3c3acbbdda60d6b39f0b5191ebde4
RohitSignIn <rohitsinghsignin@gmail.com>
2024-03-04 18:41:55 +0530

    Todo Done - Assignment Add Delete Todo Functionality

--- frontend/todo/script/main.js
+++ frontend/todo/script/main.js
@@ -1,44 +1,81 @@
-const todoInput = document.getElementById("todoInput");
-const addTodo = document.getElementById("addTodoBtn");
+$(document).ready(function () {
+  const todoInput = document.getElementById("todoInput");
+  const addTodo = document.getElementById("addTodoBtn");
 
-const todoCon = document.getElementById("todoCon");
+  const todoCon = document.getElementById("todoCon");
 
-function handleDelete(e) {
-  e.target.parentElement.parentElement.remove();
-}
+  $.ajax({
+    type: "get",
+    url: "http://localhost:9090/api/v1/todos",
+    success: function (response) {
+      for (let i = 0; i < response["res"].length; i++) {
+        console.log(response["res"][i][1]);
+        addTodoInList(response["res"][i][1]);
+      }
+    },
+  });
 
-addTodo.addEventListener("click", function () {
-  const task = todoInput.value;
+  function handleDelete(e) {
+    e.target.parentElement.parentElement.remove();
+  }
 
-  const para = document.createElement("p");
-  para.textContent = task;
+  todoInput.addEventListener("keyup", (event) => {
+    if (event.code === "Enter") {
+      addInputTodo();
+    }
+  });
 
-  const statusBtn = document.createElement("button");
-  statusBtn.textContent = "PENDING";
-  statusBtn.classList.add("btnDel");
+  addTodo.addEventListener("click", addInputTodo);
 
-  const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "DELETE";
-  deleteBtn.classList.add("btnPend");
+  function addInputTodo() {
+    $.ajax({
+      type: "get",
+      url: "http://localhost:9090/api/v1/todo",
+      data: {
+        task: todoInput.value,
+        userId: 5,
+      },
+      success: function (response) {
+        addTodoInList(todoInput.value);
+      },
+    });
+  }
 
-  paraCon = document.createElement("div");
-  statusCon = document.createElement("div");
-  deleteCon = document.createElement("div");
+  function addTodoInList(task) {
+    if (task == "") {
+      return;
+    }
 
-  paraCon.append(para);
-  statusCon.append(statusBtn);
-  deleteCon.append(deleteBtn);
+    const para = document.createElement("p");
+    para.textContent = task;
 
-  taskCon = document.createElement("div");
-  taskCon.append(paraCon);
-  taskCon.append(statusCon);
-  taskCon.append(deleteCon);
-  taskCon.classList.add("todo");
+    const statusBtn = document.createElement("button");
+    statusBtn.textContent = "PENDING";
+    statusBtn.classList.add("btnDel");
 
-  todoCon.append(taskCon);
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "DELETE";
+    deleteBtn.classList.add("btnPend");
 
-  todoInput.value = "";
+    paraCon = document.createElement("div");
+    statusCon = document.createElement("div");
+    deleteCon = document.createElement("div");
 
-  // Event Listeners
-  deleteCon.addEventListener("click", (e) => handleDelete(e));
+    paraCon.append(para);
+    statusCon.append(statusBtn);
+    deleteCon.append(deleteBtn);
+
+    taskCon = document.createElement("div");
+    taskCon.append(paraCon);
+    taskCon.append(statusCon);
+    taskCon.append(deleteCon);
+    taskCon.classList.add("todo");
+
+    todoCon.append(taskCon);
+
+    todoInput.value = "";
+
+    // Event Listeners
+    deleteCon.addEventListener("click", (e) => handleDelete(e));
+  }
 });
